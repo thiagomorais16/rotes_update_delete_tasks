af4bf4f335938b1b3016f52ad6e2c954d4ec6214
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-05-02 21:06:14 -0300

    criação doc usuario update index e autoGenDoc.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerOptions = { customCssUrl: "/swagger-ui.css" };
-const routes = require("./src/routes");
-const authDocProducao = require("./src/middlewares/authDoc");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
+const routes = require('./src/routes');
+const authDocProducao = require('./src/middlewares/authDoc');
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-if (process.env.NODE_ENV !== 'test') {
-  const swaggerFile = require("./swagger/swagger_output.json");
-
+if(process.env.NODE_ENV !== 'test'){
+  const swaggerFile = require('./swagger/swagger_output.json');
   app.get('/', (req, res) => {
     /* #swagger.ignore = true */ res.redirect('/doc');
   });
@@ -31,7 +30,7 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 routes(app);
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -1,4 +1,5 @@
 const mongooseToSwagger = require('mongoose-to-swagger');
+const EsquemaUsuario = require('../src/models/usuario.js');
 const swaggerAutogen = require('swagger-autogen')({
     openapi:'3.0.0',
     language:'pt-Br',
@@ -8,23 +9,27 @@ const endpointsFiles = ['./index.js', './src/routes.js'];
 
 let doc = {
   info: {
-      version: "1.0.0",
-      title: "API do Tarefas",
-      description: "Documentação da API do Tarefas."
+    version: "1.0.0",
+    title: "API do Tarefas",
+    description: "Documentação da API do Tarefas.",
   },
   servers: [
-      {
-          url: "http://localhost:4000/",
-          description: "Servidor localhost."
-      },
-      {
-          url: "https://tarefas-back.vercel.app/",
-          description: "Servidor de produção."
-      }
+    {
+      url: "http://localhost:4000/",
+      description: "Servidor localhost.",
+    },
+    {
+      url: "https://tarefas-back.vercel.app/",
+      description: "Servidor de produção.",
+    },
   ],
   consumes: ['application/json'],
-    produces: ['application/json']
-
+  produces: ['application/json'],
+  components: {
+    schemas:{
+          Usuario: mongooseToSwagger(EsquemaUsuario) 
+      }
+   }
 }
 
 
