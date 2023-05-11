c04d872ea9fb8830c224fe6259e363e08ab0489a
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-05-11 16:43:28 -0300

    criação do middleware de verificação do token

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -7,22 +7,22 @@ const EsquemaUsuario = require('../models/usuario');
 const router = express.Router();
 
 
-router.post('/criar', conectarBancoDados, async function(req, res) {
+router.post('/criar', conectarBancoDados, async function (req, res) {
   try {
     // #swagger.tags = ['Usuario']
-    let {nome, email, senha} = req.body;
+    let { nome, email, senha } = req.body;
     const numeroVezesHash = 10;
     const senhaHash = await bcrypt.hash(senha, numeroVezesHash);
-    const respostaBD = await EsquemaUsuario.create({nome, email, senha: senhaHash});
+    const respostaBD = await EsquemaUsuario.create({ nome, email, senha: senhaHash });
 
     res.status(200).json({
       status: "OK",
       statusMensagem: "Usuário criado com sucesso.",
       resposta: respostaBD
-  })
+    })
 
-} catch (error) {
-    if(String(error).includes("email_1 dup key")){
+  } catch (error) {
+    if (String(error).includes("email_1 dup key")) {
       return tratarErrosEsperados(res, "Error: Já existe uma conta com esse e-mail!");
     }
     return tratarErrosEsperados(res, error);
@@ -52,7 +52,7 @@ router.post('/logar', conectarBancoDados, async function (req, res) {
         });
       } else {
         throw new Error("Email ou senha incorreta");
-      } 
+      }
     } else {
       throw new Error("Email ou senha incorreta");
     }
