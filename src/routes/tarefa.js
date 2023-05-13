0bdb65e512cd9f64ffa50e9679b345ca2ece9b56
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-05-13 08:54:58 -0300

    Criação da rota de tarefas

---
+++ src/routes/tarefa.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
+const conectarBancoDados = require('../middlewares/conectarBD');
+const EsquemaTarefa = require('../models/tarefa');
+const authUser = require('../middlewares/authUser');
+const router = express.Router();
+
+
+router.post('/criar', authUser, conectarBancoDados, async function (req, res) {
+    try {
+        // #swagger.tags = ['Tarefa']
+        let { posicao, título, descricao, status, dataEntrega } = req.body;
+        const usuarioCriador = req.usuarioJwt.id;
+        const respostaBD = await EsquemaTarefa.create({ posicao, título, descricao, status, dataEntrega, usuarioCriador });
+
+        res.status(200).json({
+            status: "OK",
+            statusMensagem: "Tarefa criado com sucesso.",
+            resposta: respostaBD
+        })
+
+    } catch (error) {
+        return tratarErrosEsperados(res, error);
+    }
+});
+
+
+
+
+
+module.exports = router;
+
