12b0c3fc108f1b2b8f90c2f02b6d6a793ad6ffe6
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-05-14 14:38:45 -0300

    update tarefa.js

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -25,6 +25,31 @@ router.post('/criar', authUser, conectarBancoDados, async function (req, res) {
 });
 
 
+router.put('/editar/:id', authUser, conectarBancoDados, async function (req, res) {
+    try {
+        // #swagger.tags = ['Tarefa']
+        let idTarefa = req.params.id;
+        let { posicao, título, descricao, status, dataEntrega } = req.body;
+        const usuarioLogado = req.usuarioJwt.id;
+
+        const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
+        if (!checkTarefa) {
+            throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+        }
+
+        const respostaBD = await EsquemaTarefa.updateOne({ _id: idTarefa }, { posicao, titulo, descricao, status, dataEntrega });
+
+        res.status(200).json({
+            status: "OK",
+            statusMensagem: "Tarefa atualizada com sucesso.",
+            resposta: respostaBD
+        })
+
+    } catch (error) {
+        return tratarErrosEsperados(res, error);
+    }
+});
+
 
 
 
